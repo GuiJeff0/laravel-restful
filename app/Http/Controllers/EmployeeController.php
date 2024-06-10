64f47691ee49e4d360713eb6c4649a1e77a29342
creedx <guilhermejeffofc@gmail.com>
2024-06-10 16:19:55 -0300

    Update EmployeeController

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -16,9 +16,15 @@ class EmployeeController extends Controller
     }
 
 
+    // get all employee
     public function index()
     {
-        
+        try{
+            $getAllEmployee = $this->repositoryInterface->getallemployee();
+            return response()->json(['data' => $getAllEmployee]);
+        } catch (\Exception $e) {
+            return response()->json(['error'=> $e->getMessage(), 500]);
+        }
     }
 
     
@@ -31,8 +37,8 @@ class EmployeeController extends Controller
                 'phone' => 'required|string'             
             ]);
 
-            $createtable = $this->repositoryInterface->store($data);
-            return response()->json(['data' => $createtable]);
+            $createTable = $this->repositoryInterface->store($data);
+            return response()->json(['data' => $createTable]);
         } catch (\Exception $e){
             return response()->json(['error'=> $e->getMessage()],500);
         }
@@ -46,12 +52,28 @@ class EmployeeController extends Controller
    
     public function update(Request $request, string $id)
     {
-        
+        try{
+            $data = $request->validate([
+                'employeename' => 'required|string',
+                'address' => 'required|string',
+                'phone' => 'required|string'             
+            ]);
+
+            $updateEmployee = $this->repositoryInterface->updateemployee($id, $data);
+            return response()->json(['data'=> $updateEmployee]);
+        } catch (\Exception $e){
+            return response()->json(['error'=> $e->getMessage()],500);
+        }
     }
 
    
     public function destroy(string $id)
-    {
-        
+    {   
+        try{
+            $deleteEmployee = $this->repositoryInterface->deleteemployee($id);
+            return response()->json(['data'=> $deleteEmployee]);
+        } catch (\Exception $e){
+            return response()->json(['error'=> $e->getMessage()],500);
+        }
     }
 }
